7a69948811c3b2e05511f71f58c80010d79b556c
Li-ionFractalNanocore <liionfractalnanocore@gmail.com>
2024-10-20 10:20:42 +0800

    add: 908

---
+++ exercise/greedy/908/1.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        min_num = min(nums)
+        max_num = max(nums)
+        diff = max_num - min_num
+        if diff <= 2 * k:
+            return 0
+        else:
+            return diff - 2 * k
+            
+        
