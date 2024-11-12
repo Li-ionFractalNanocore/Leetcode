3b0e0a07eef6ed7005f416e0c14bae33a892391d
Li-ionFractalNanocore <liionfractalnanocore@gmail.com>
2024-11-12 15:36:45 +0800

    add: 303

---
+++ exercise/prefix/303/1.py
@@ -0,0 +1,18 @@
+from typing import List
+from itertools import accumulate
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = list(accumulate(nums, initial=0))
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
