7f48b6adab22d68101294c83323c6ebe33544d3b
Li-ionFractalNanocore <liionfractalnanocore@gmail.com>
2025-02-20 20:22:46 +0800

    add: 11

---
+++ exercise/greedy/11/1.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        area = min(height[left], height[right]) * (right - left)
+        while left < right:
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+            area = max(area, min(height[left], height[right]) * (right - left))
+        return area
+        
